05c8f9c4b362abcc1bb60dc19b3b5ebed1eafb4a
Sasha Savchuk <mrxlinch@gmail.com>
2020-03-31 23:52:29 +0300

    trigger build

--- client_test.go
+++ client_test.go
@@ -9,25 +9,25 @@ import (
 
 func TestNewClient(t *testing.T) {
 	const (
-		token = "12345:secret"
+		toke_n = "12345:secret"
 	)
 
 	t.Run("WithoutOptions", func(t *testing.T) {
-		client := NewClient(token)
+		client := NewClient(toke_n)
 
 		if assert.NotNil(t, client) {
-			assert.Equal(t, token, client.token)
+			assert.Equal(t, toke_n, client.token)
 			assert.Equal(t, http.DefaultClient, client.doer)
 		}
 	})
 
 	t.Run("WithOptions", func(t *testing.T) {
 		doer := &http.Client{}
-		clien_t := NewClient(token, WithDoer(doer))
+		client := NewClient(token, WithDoer(doer))
 
-		if assert.NotNil(t, clien_t) {
-			assert.Equal(t, token, clien_t.token)
-			assert.Equal(t, doer, clien_t.doer)
+		if assert.NotNil(t, client) {
+			assert.Equal(t, token, client.token)
+			assert.Equal(t, doer, client.doer)
 		}
 	})
 }

d07709541117c0f5e413fbd91ae1e73ff679be8f
Sasha Savchuk <mrxlinch@gmail.com>
2020-04-30 18:33:20 +0300

    input file & request

--- request.go
+++ request.go
@@ -1,1 +1,34 @@
 package tg
+
+// Request represents RPC request to Telegram Bot API.
+// It contains following info: method, token, parameters, files.
+type Request struct {
+	token  string
+	method string
+
+	args          map[string]string
+	files         map[string]InputFile
+	attachmentIdx int
+}
+
+// NewRequest creates request with provided method.
+func NewRequest(method string) *Request {
+	return &Request{
+		method: method,
+	}
+}
+
+// Token returns request token.
+func (r *Request) Token() string {
+	return r.token
+}
+
+// Method returns request method.
+func (r *Request) Method() string {
+	return r.method
+}
+
+// SetToken sets request token.
+func (r *Request) SetToken(token string) {
+	r.token = token
+}
--- request_test.go
+++ request_test.go
@@ -1,1 +1,27 @@
 package tg
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewRequest(t *testing.T) {
+	r := NewRequest("getMe")
+
+	if assert.NotNil(t, r) {
+		assert.Equal(t, "getMe", r.Method())
+	}
+}
+
+func TestRequestToken(t *testing.T) {
+	r := NewRequest("getMe")
+
+	if assert.NotNil(t, r) {
+		assert.Equal(t, "", r.Token())
+
+		r.SetToken("1234:test")
+
+		assert.Equal(t, "1234:test", r.Token())
+	}
+}

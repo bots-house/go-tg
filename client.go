cb83f2cc18b777e61714403edc2c3f523cd5f339
Sasha Savchuk <mrxlinch@gmail.com>
2020-04-30 18:08:57 +0300

    restructure client, use doer, idea in ignore

--- client.go
+++ client.go
@@ -2,21 +2,29 @@ package tg
 
 import "net/http"
 
-// Client it's Telegram Bot API client.
-type Client struct {
-	token string
-	doer  *http.Client
+
+// Doer define interface for mock client calls.
+type Doer interface {
+	// Do HTTP request.
+	Do(r *http.Request) (*http.Response, error)
 }
 
-// ClientOption define type for custumize client.
+// ClientOption define type for customize client.
 type ClientOption func(*Client)
 
 // WithDoer sets client http doer.
-func WithDoer(doer *http.Client) ClientOption {
+func WithDoer(doer Doer) ClientOption {
 	return func(client *Client) {
 		client.doer = doer
 	}
 }
+
+// Client it's Telegram Bot API client.
+type Client struct {
+	token string
+	doer  Doer
+}
+
 
 // NewClient creates new Telegram Bot API client with provided token.
 // Additional options can be specified using ClientOption.
